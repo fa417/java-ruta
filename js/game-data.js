36d3a3909bb508c598a0888d95fb2a82ae73a4d2
fa417 <ts.yome@gmail.com>
2025-08-04 05:44:44 +0900

    fix

--- js/game-data.js
+++ js/game-data.js
@@ -54,21 +54,7 @@ function loadScore() {
 }
 
 const select = document.getElementById('month-choice');
-// ダミー
-const dummyJuly = {
-  "2025-07-01": 10,
-  "2025-07-05": 16,
-  "2025-07-15": 20
-};
 
-const dummyAugust = {
-  "2025-08-02": 8,
-  "2025-08-07": 16,
-  "2025-08-15": 20
-};
-
-localStorage.setItem("2025-07", JSON.stringify(dummyJuly));
-localStorage.setItem("2025-08", JSON.stringify(dummyAugust));
 const allKey = Object.keys(localStorage);
 
 select.innerHTML = '';
